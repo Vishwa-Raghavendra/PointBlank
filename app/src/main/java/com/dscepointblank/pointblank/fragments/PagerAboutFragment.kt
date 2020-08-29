e0b1cb3b7bac51bd9942b39c1c8e71d8660d15fc
coder2699 <pranshu0414@gmail.com>
2020-08-29 14:56:41 +0530

    Added About Fragment

--- app/src/main/java/com/dscepointblank/pointblank/fragments/PagerAboutFragment.kt
+++ app/src/main/java/com/dscepointblank/pointblank/fragments/PagerAboutFragment.kt
@@ -1,32 +1,33 @@
 package com.dscepointblank.pointblank.fragments
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.dscepointblank.pointblank.R
 import com.dscepointblank.pointblank.adapters.ViewPagerAdapter
+import com.dscepointblank.pointblank.fragments.AboutViewPager.*
 import com.dscepointblank.pointblank.fragments.onbording.CodingContests
 import com.dscepointblank.pointblank.fragments.onbording.Forum
 import com.dscepointblank.pointblank.fragments.onbording.WriteUpFragment
 import com.dscepointblank.pointblank.utilityClasses.DepthPageTransformer
 import kotlinx.android.synthetic.main.fragment_view_pager.view.*
 
-class ViewPagerFragment : Fragment() {
-
-
+class PagerAboutFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
-
+        val view= inflater.inflate(R.layout.fragment_pager_about, container, false)
         val fragList = arrayListOf<Fragment>(
-            Forum(),
-            WriteUpFragment(),
-            CodingContests()
+            Pic1Fragment(),
+            Pic2Fragment(),
+            Pic3Fragment(),
+            Pic4Fragment(),
+            Pic5Fragment(),
+            Pic6Fragment()
         )
 
         val adapter =
@@ -35,9 +36,6 @@ class ViewPagerFragment : Fragment() {
         val viewPager2 = view.viewPager
         viewPager2.adapter = adapter
         viewPager2.setPageTransformer(DepthPageTransformer())
-
         return view
     }
-
-
 }
